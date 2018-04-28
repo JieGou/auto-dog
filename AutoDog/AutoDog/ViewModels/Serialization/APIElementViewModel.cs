e1123f5767b8ce31153a5bc145807d5dc861ff37
devdiv <haizhibin1989>
2018-04-28 15:19:39 +0800

    update

---
+++ AutoDog/AutoDog/ViewModels/Serialization/APIElementViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows.Markup;
+using System.Xml.Serialization;
+using Standard;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace AutoDog.ViewModels.Serialization
+{
+    [ContentProperty("APITestCase")]
+    [Serializable]
+    public class APIElements :ViewModelBase , IXmlSerializable
+    {
+        public APIElements()
+        {
+
+        }
+
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            reader.MoveToContent();
+            if (reader.IsEmptyElement)
+            {
+                reader.Read();
+                return;
+            }
+        }
+
+        public string APITestCase{ get; set; }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("APITestCase");
+            if (APITestCase != null)
+            {
+                //APITestCase.WriteXml(writer);
+            }
+            writer.WriteEndElement();
+
+            // Write all floating windows (can be LayoutDocumentFloatingWindow or LayoutAnchorableFloatingWindow).
+            // To prevent "can not create instance of abstract type", the type is retrieved with GetType().Name
+            //writer.WriteStartElement("FloatingWindows");
+            //foreach (var layoutFloatingWindow in FloatingWindows)
+            //{
+            //    writer.WriteStartElement(layoutFloatingWindow.GetType().Name);
+            //    layoutFloatingWindow.WriteXml(writer);
+            //    writer.WriteEndElement();
+            //}
+            //writer.WriteEndElement();
+
+            //writer.WriteStartElement("Hidden");
+            //foreach (var layoutAnchorable in Hidden)
+            //{
+            //    layoutAnchorable.WriteXml(writer);
+            //}
+            //writer.WriteEndElement();
+        }
+    }
+}
